6cf8a8a782cfbccbd228da86d7db9c016955ee3a
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-01-30 12:02:46 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express';
 import dotenv from "dotenv"
 import cors from "cors"
@@ -11,6 +12,7 @@ import connectToDb from './db/connect_to_db.js';
 import { app, server } from './socket/socket.js';
 
 
+const __dirname = path.resolve()
 
 
 const PORT = process.env.PORT || 5000 ;
@@ -23,6 +25,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
 app.use(express.urlencoded({extended : true})) ;
 app.use(cookieParser());
 
@@ -30,6 +33,10 @@ app.use(cookieParser());
 app.use("/api/auth" , authRoutes );
 app.use("/api/messages" , messageRoutes );
 app.use("/api/users" , userRoutes)
+
+app.get("*" , (req , res ) => {
+    res.sendFile(path.join(__dirname, "frontend" , "dist" , "index.html"))
+})
 
 server.listen(PORT ,() => {
     connectToDb() 
